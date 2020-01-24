b0b9262e4aa1dea00e5896a92adbab1bf50b1817
ipaullly <ipaully22@gmail.com>
2020-01-24 14:23:00 +0300

    finish instant game

--- script/main.js
+++ script/main.js
@@ -10,50 +10,137 @@ FBInstant.initializeAsync()
         FBInstant.startGameAsync()
           .then(() => {
             console.log('Game has started');
-            const getEl = (id) => {
-              return document.getElementById(id);
-            };
-            
-            let startButton = getEl("startGame");
-            
-            let rock = getEl("rock");
-            let paper = getEl("paper");
-            let scissors = getEl("scissors");
-            
-            let buttonGroup = getEl("buttongroup");
-            
-            const startGame = () => {
-              showButtons();
-              hideStartButton();
-            }
-            
-            startButton.addEventListener('click', startGame);
-            
-            const showButtons = () => {
-              if (buttonGroup.classList.contains("hidden")) {
-                buttonGroup.classList.remove("hidden");
-              }
-            }
-            
-            const hideButtons = () => {
-              if (!buttonGroup.classList.contains("hidden")) {
-                buttonGroup.classList.add("hidden");
-              }
-            }
-            
-            const showStartButton = () => {
-              if (startButton.classList.contains("hidden")) {
-                startButton.classList.remove("hidden");
-              }
-            }
-            
-            const hideStartButton = () => {
-              if (!startButton.classList.contains("hidden")) {
-                startButton.classList.add("hidden");
-              }
-            }
           });
       }
     }, 10)
     console.log("loaded");
   });
+
+  const getEl = (id) => {
+    return document.getElementById(id);
+  };
+  
+  let startButton = getEl("startGame");
+  let gamePlay = getEl("gamePlay");
+  
+  let playButtons = document.getElementsByClassName("playbutton");
+
+  let rock = getEl("rock");
+  let paper = getEl("paper");
+  let scissors = getEl("scissors");
+  
+  let compChooses = getEl("compChooses");
+  let compChoosesParent = getEl("compChoosesParent");
+  let buttonGroup = getEl("buttongroup");
+
+  let choices = ["rock", "scissors", "paper"];
+
+  let playerSc = getEl("playerSc");
+  let compSc = getEl("compSc");
+
+  let wins = {
+    "rocks": "scissors",
+    "scissors": "paper",
+    "paper": "rock"
+  }
+
+  let playerScore = 0;
+  let compScore = 0;
+
+  const startGame = () => {
+    showButtons();
+    hideStartButton();
+    showGamePlay();
+    hideCompChoosesParent();
+  }
+
+  const buttonClicked = (e) => {
+    let btnChoice = e.target.id;
+    playChoice(btnChoice);
+  }
+  
+  startButton.addEventListener('click', startGame);
+
+  Array.from(playButtons).forEach(btn => {
+    btn.addEventListener("click", buttonClicked);
+  });
+
+  const playChoice = (btnChoice) => {
+    let compChoice = choices[Math.floor(Math.random() * choices.length)];
+    displayComputerChoice(compChoice);
+    console.log(compChoice);
+    
+    if (wins[btnChoice] == compChoice) {// win for player
+      playerScore++;
+    } else if (btnChoice === compChoice) {
+      return;
+    }
+    else {
+      compScore++;
+    }
+    updateScore();
+  }
+
+  const displayComputerChoice = (ch) => {
+    showCompChoosesParent();
+    let emoji = "";
+    switch (ch) {
+      case "paper": emoji = "✋"; break;
+      case "rock": emoji = "✊"; break;
+      case "scissors": emoji = "✌"; break;
+    }
+    compChooses.innerHTML = emoji
+  }
+  
+  const updateScore = () => {
+    playerSc.innerHTML = playerScore;
+    compSc.innerHTML = compScore;
+  }
+
+  const showButtons = () => {
+    if (buttonGroup.classList.contains("hidden")) {
+      buttonGroup.classList.remove("hidden");
+    }
+  }
+  
+  const hideButtons = () => {
+    if (!buttonGroup.classList.contains("hidden")) {
+      buttonGroup.classList.add("hidden");
+    }
+  }
+  
+  const showStartButton = () => {
+    if (startButton.classList.contains("hidden")) {
+      startButton.classList.remove("hidden");
+    }
+  }
+  
+  const hideStartButton = () => {
+    if (!startButton.classList.contains("hidden")) {
+      startButton.classList.add("hidden");
+    }
+  }
+
+  const showGamePlay = () => {
+    if (gamePlay.classList.contains("hidden")) {
+      gamePlay.classList.remove("hidden");
+    }
+  }
+  
+  const hideGamePlay = () => {
+    if (!gamePlay.classList.contains("hidden")) {
+      gamePlay.classList.add("hidden");
+    }
+  }
+
+  const showCompChoosesParent = () => {
+    if (compChoosesParent.classList.contains("hidden")) {
+      compChoosesParent.classList.remove("hidden");
+    }
+  }
+  
+  const hideCompChoosesParent = () => {
+    if (!compChoosesParent.classList.contains("hidden")) {
+      compChoosesParent.classList.add("hidden");
+    }
+  }
